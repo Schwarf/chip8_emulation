4e45bd5ffa7e6a90636aa532d027bee7ecddc802
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-02-09 18:40:26 +0100

    Add sprites.

--- chip8/chip8.h
+++ chip8/chip8.h
@@ -11,26 +11,52 @@ template<size_t memory_in_bytes, size_t number_of_registers, size_t width_in_pix
 	size_t number_of_stack_levels, size_t number_of_keys>
 class Chip8
 {
+	using Bit16 = unsigned short;
+	using Bit8 = unsigned char;
 
 public:
-	Chip8(){}
-	
-	void initialize()
-	{
+	Chip8(){
+		initializeSprites();
 	}
 
+
 private:
-	std::array<unsigned char, memory_in_bytes> memory{};
-	std::array<unsigned char, number_of_registers> registers{};
-	std::array<unsigned char, width_in_pixels * height_in_pixels> graphics{};
-	std::array<unsigned char, number_of_stack_levels> stack{};
-	std::array<unsigned char, number_of_keys> keypad{};
-	unsigned short current_opcode{};
-	unsigned short index_register{};
-	unsigned char stack_pointer{};
-	unsigned short program_counter = 0x200;
-	unsigned char delayed_timer{};
-	unsigned char sound_timer{};
+	void initializeSprites()
+	{
+		std::array<Bit8, 80> sprites = {
+			0xF0, 0x90, 0x90, 0x90, 0xF0, // "0"
+			0x20, 0x60, 0x20, 0x20, 0x70, // "1"
+			0xF0, 0x10, 0xF0, 0x80, 0xF0, // "2"
+			0xF0, 0x10, 0xF0, 0x10, 0xF0, // "3"
+			0x90, 0x90, 0xF0, 0x10, 0x10, // "4"
+			0xF0, 0x80, 0xF0, 0x10, 0xF0, // "5"
+			0xF0, 0x80, 0xF0, 0x90, 0xF0, // "6"
+			0xF0, 0x10, 0x20, 0x40, 0x40, // "7"
+			0xF0, 0x90, 0xF0, 0x90, 0xF0, // "8"
+			0xF0, 0x90, 0xF0, 0x10, 0xF0, // "9"
+			0xF0, 0x90, 0xF0, 0x90, 0x90, // "A"
+			0xE0, 0x90, 0xE0, 0x90, 0xE0, // "B"
+			0xF0, 0x80, 0x80, 0x80, 0xF0, // "C"
+			0xE0, 0x90, 0x90, 0x90, 0xE0, // "D"
+			0xF0, 0x80, 0xF0, 0x80, 0xF0, // "E"
+			0xF0, 0x80, 0xF0, 0x80, 0x80  // "F"
+		};
+
+		std::copy(sprites.begin(), sprites.end(), memory.begin());
+	}
+
+
+	std::array<Bit8, memory_in_bytes> memory{};
+	std::array<Bit8, number_of_registers> registers{};
+	std::array<Bit8, width_in_pixels * height_in_pixels> graphics{};
+	std::array<Bit8, number_of_stack_levels> stack{};
+	std::array<Bit8, number_of_keys> keypad{};
+	Bit16 current_opcode{};
+	Bit16 index_register{};
+	Bit16 program_counter = 0x200;
+	Bit8 stack_pointer{};
+	Bit8 delayed_timer{};
+	Bit8 sound_timer{};
 
 
 
