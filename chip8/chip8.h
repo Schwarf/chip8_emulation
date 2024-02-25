24ba4965d8acdfd981cff88105e1648785731d97
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-02-25 19:15:40 +0100

    Refactoring: Use lambdas instead of small void functions.

--- chip8/chip8.h
+++ chip8/chip8.h
@@ -60,7 +60,8 @@ public:
 		current_opcode = 0;
 		index_register = 0;
 		stack_pointer = 0;
-		clearScreen();
+		// clear graphics
+		std::fill(graphics.begin(), graphics.end(), 0);
 		std::fill(stack.begin(), stack.end(), 0);
 		std::fill(registers.begin(), registers.end(), 0);
 		std::fill(memory.begin(), memory.end(), 0);
@@ -85,196 +86,196 @@ public:
 			--sound_timer;
 		}
 
-		if(skip_instruction) {
+		if (skip_instruction) {
 			program_counter += 4;
 			skip_instruction = false;
 		}
-		else if(advance_program_counter)
-		{
-			program_counter +=2;
+		else if (advance_program_counter) {
+			program_counter += 2;
 		}
-		else
-		{
+		else {
 			advance_program_counter = true;
 		}
 	}
 
 
 private:
-	void clearScreen()
-	{
-		std::fill(graphics.begin(), graphics.end(), 0);
-	}
-
-	void returnFromSubroutine()
-	{
-		--stack_pointer;
-		program_counter = stack[stack_pointer];
-	}
-
-	void jumpToAddress()
-	{
-		program_counter = current_opcode & 0x0FFF;
-		advance_program_counter = false;
-	}
-
-	void jumpToSubroutine()
-	{
-		stack[stack_pointer] = program_counter;
-		++stack_pointer;
-		program_counter = current_opcode & 0x0FFF;
-		advance_program_counter = false;
-	}
-
-	// 0x3XNN: Skips the next instruction if VX equals NN
-	void skipNextInstructionIfRegisterDoesMatch()
-	{
-		const int value = (current_opcode & 0x00FF);
-		if (value == registers[register_index1]) {
-			skip_instruction = true;
-		}
-	}
-
-	// 0x4XNN: Skips the next instruction if VX NOT equals NN
-	void skipNextInstructionIfRegisterDoesNotMatch()
-	{
-		const int value = (current_opcode & 0x00FF);
-		if (value != registers[register_index1]) {
-			skip_instruction = true;
-		}
-	}
-
-	void skipNextInstructionIfRegisterMatch()
-	{
-		if (registers[register_index1] == registers[register_index2]) {
-			skip_instruction = true;
-		}
-	}
-
-	void setValueInRegister()
-	{
-		const int value_to_set = (current_opcode & 0x00FF);
-		registers[register_index1] = value_to_set;
-	}
-
-	void addsValueToRegister()
-	{
-		const int value_to_add = (current_opcode & 0x00FF);
-		registers[register_index1] += value_to_add;
-	}
-
 	void twoRegisterOperations()
 	{
 		const int operation_index = (current_opcode & 0x000F);
-		if(twoRegisterOperationsMap.find(operation_index) == twoRegisterOperationsMap.end())
-		{
-			std::cerr << "Invalid operations_index for two register operations: "<< operation_index << std::endl;
+		if (twoRegisterOperationsMap.find(operation_index) == twoRegisterOperationsMap.end()) {
+			std::cerr << "Invalid operations_index for two register operations: " << operation_index << std::endl;
 			return;
 		}
-		twoRegisterOperationsMap[operation_index](register_index1, register_index2);
-	}
-
-	void setIndexRegister()
-	{
-		index_register = current_opcode & 0x0FFF;
-	}
-
-	void jumpToAddressPlusRegisterZero()
-	{
-		program_counter = (current_opcode & 0x0FFF) + registers[0];
-		advance_program_counter = false;
-	}
-
-	void setRegisterToRandomValue()
-	{
-		registers[register_index1] = (rand() % 0xFF)	& (current_opcode & 0x00FF);
+		twoRegisterOperationsMap[operation_index]();
 	}
 
 	void skipInstructionKeyRegister()
 	{
 		int decision = current_opcode & 0x00FF;
 		// EX9E: Skips the next instruction if the key stored in register is pressed
-		if(decision == 0x009E && keypad[registers[register_index1]] != 0) {
-				program_counter += 2;
-		}
-			// EX9E: Skips the next instruction if the key stored in register is NOT pressed
-		else if(decision == 0x00A1  && keypad[registers[register_index1]] == 0)
-		{
+		if (decision == 0x009E && keypad[registers[register_index1]] != 0) {
 			program_counter += 2;
 		}
-		else
-		{
+			// EX9E: Skips the next instruction if the key stored in register is NOT pressed
+		else if (decision == 0x00A1 && keypad[registers[register_index1]] == 0) {
+			program_counter += 2;
+		}
+		else {
 			std::cerr << "Unknown instruction in 'skopInstructionKeyRegister'" << std::endl;
 		}
 	}
 
-
 	void initializeTwoRegisterOperations()
 	{
 		twoRegisterOperationsMap[0] = [this]()
-		{ this->oneEqualTwo(); };
+		{ registers[register_index1] = registers[register_index2]; };
 		twoRegisterOperationsMap[1] = [this]()
-		{ this->oneEqualOneOrTwo(); };
+		{ registers[register_index1] = registers[register_index2] | registers[register_index1]; };
 		twoRegisterOperationsMap[2] = [this]()
-		{ this->oneEqualOneAndTwo(); };
+		{registers[register_index1] = registers[register_index2] & registers[register_index1];};
 		twoRegisterOperationsMap[3] = [this]()
-		{ this->oneEqualOneXorTwo(); };
+		{ registers[register_index1] = registers[register_index2] ^ registers[register_index1]; };
 		twoRegisterOperationsMap[4] = [this]()
-		{ this->oneEqualOnePlusTwo(); };
+		{
+			// max value is 0xFF. if value stored in index2 is greater than 0xFF minus value stored in index1 we have a carry
+			registers[number_of_registers - 1] = 0;
+			if (registers[register_index2] > 0xFF - registers[register_index1]) {
+				registers[number_of_registers - 1] = 1;// set carry
+			}
+			registers[register_index1] += registers[register_index2];
+		};
 		twoRegisterOperationsMap[5] = [this]()
-		{ this->oneEqualOneMinusTwo(); };
+		{
+			// if value in register_index2 is larger than value stored at register_index1 then we have a borrow
+			registers[number_of_registers - 1] = 1;
+			if (registers[register_index2] > registers[register_index1]) {
+				registers[number_of_registers - 1] = 0;// set borrow
+			}
+			registers[register_index1] -= registers[register_index2];
+		};
 		twoRegisterOperationsMap[6] = [this]()
-		{ this->oneEqualRightShiftOne(); };
+		{
+			// Shifts value at index1 right by one. The last register is set to the value of the least significant bit of the register with index1 before the shift
+			//  We store least significant bit of
+			registers[number_of_registers - 1] = registers[register_index1] & 0x1;
+			registers[register_index1] >>= 1;
+
+		};
 		twoRegisterOperationsMap[7] = [this]()
-		{ this->oneEqualTwoMinusOne(); };
+		{
+			// Shifts value at index1 right by one. The last register is set to the value of the most significant bit of the register with index1 before the shift
+			//  We store most significant bit of
+			registers[number_of_registers - 1] = registers[register_index1] >> 7;
+			registers[register_index1] <<= 1;
+
+		};
 		twoRegisterOperationsMap[14] = [this]()
-		{ this->oneEqualLeftShiftOne(); };
+		{
+			registers[number_of_registers - 1] = 1;
+			if (registers[register_index1] > registers[register_index2]) {
+				registers[number_of_registers - 1] = 0;// set borrow
+			}
+			registers[register_index1] = registers[register_index2] - registers[register_index1];
+
+		};
 
 	}
 
 	void initializeOpcodeMap()
 	{
+		// clear screen
 		opcodeMap[0x00E0] = [this]()
-
-		{ this->clearScreen(); };
+		{
+			std::fill(graphics.begin(), graphics.end(), 0);
+		};
+		// return from subroutine
 		opcodeMap[0x00EE] = [this]()
-		{ this->returnFromSubroutine(); };
+		{
+			--stack_pointer;
+			program_counter = stack[stack_pointer];
+		};
+		// jump to address
 		opcodeMap[0x1000] = [this]()
-		{ this->jumpToAddress(); };
+		{
+			program_counter = current_opcode & 0x0FFF;
+			advance_program_counter = false;
+		};
 		// 0x2NNN: Calls subroutine at NNN.
 		opcodeMap[0x2000] = [this]()
-		{ this->jumpToSubroutine(); };
+		{
+			stack[stack_pointer] = program_counter;
+			++stack_pointer;
+			program_counter = current_opcode & 0x0FFF;
+			advance_program_counter = false;
+		};
 		// 0x3XNN: Skips the next instruction if VX equals NN
 		opcodeMap[0x3000] = [this]()
-		{ this->skipNextInstructionIfRegisterDoesMatch(); };
+		{
+			const int value = (current_opcode & 0x00FF);
+			if (value == registers[register_index1]) {
+				skip_instruction = true;
+			}
+		};
 		// 0x4XNN: Skips the next instruction if VX NOT equals NN
 		opcodeMap[0x4000] = [this]()
-		{ this->skipNextInstructionIfRegisterDoesNotMatch(); };
+		{
+			const int value = (current_opcode & 0x00FF);
+			if (value != registers[register_index1]) {
+				skip_instruction = true;
+			}
+		};
 		// 0x5XY0: Skips the next instruction if VX equals VY.
 		opcodeMap[0x5000] = [this]()
-		{ this->skipNextInstructionIfRegisterMatch(); };
+		{
+			if (registers[register_index1] == registers[register_index2]) {
+				skip_instruction = true;
+			}
+
+		};
 		// 0x6XNN: Sets VX to NN.
 		opcodeMap[0x6000] = [this]()
-		{ this->setValueInRegister(); };
+		{
+			const int value_to_set = (current_opcode & 0x00FF);
+			registers[register_index1] = value_to_set;
+
+		};
 		// 0x7XNN: Adds NN to VX.
 		opcodeMap[0x7000] = [this]()
-		{ this->addsValueToRegister(); };
+		{
+			const int value_to_add = (current_opcode & 0x00FF);
+			registers[register_index1] += value_to_add;
+
+		};
 		// 0x8XYZ: Two register operations
 		opcodeMap[0x8000] = [this]()
-		{ this->twoRegisterOperations(); };
+		{
+			this->twoRegisterOperations();
+		};
 		opcodeMap[0x9000] = [this]()
-		{ this->skipNextInstructionIfRegisterDoesNotMatch(); };
+		{
+			if (registers[register_index1] != registers[register_index2])
+				skip_instruction = true;
+		};
+		// ANNN: Set index_register to address
 		opcodeMap[0xA000] = [this]()
-		{ this->setIndexRegister(); };
+		{
+			index_register = current_opcode & 0x0FFF;
+		};
+		// BNNN: Jumps to the address NNN plus register 0
 		opcodeMap[0xB000] = [this]()
-		{ this->jumpToAddressPlusRegisterZero(); };
+		{
+			program_counter = (current_opcode & 0x0FFF) + registers[0];
+			advance_program_counter = false;
+		};
 		opcodeMap[0xC000] = [this]()
-		{ this->setRegisterToRandomValue(); };
+		{
+			registers[register_index1] = (rand() % 0xFF) & (current_opcode & 0x00FF);
+		};
 		opcodeMap[0xD000] = [this]()
 		{ this->drawASprite(); };
 		opcodeMap[0xE000] = [this]()
 		{ this->skipInstructionKeyRegister(); };
-
 
 	}
 
@@ -283,16 +284,13 @@ private:
 		auto x = registers[register_index1];
 		auto y = registers[register_index2];
 		unsigned short height = current_opcode & 0x000F;
-		registers[number_of_registers-1] = 0;
-		for(int y_line{}; y_line< height; ++y_line)
-		{
+		registers[number_of_registers - 1] = 0;
+		for (int y_line{}; y_line < height; ++y_line) {
 			unsigned short pixel = memory[index_register + y_line];
-			for(int x_line{}; x_line< height; ++x_line) {
-				if((pixel & (0x80 >> x_line)) != 0)
-				{
-					if(graphics[(x + x_line + ((y + y_line) * 64))] == 1)
-					{
-						registers[number_of_registers-1] = 1;
+			for (int x_line{}; x_line < height; ++x_line) {
+				if ((pixel & (0x80 >> x_line)) != 0) {
+					if (graphics[(x + x_line + ((y + y_line) * 64))] == 1) {
+						registers[number_of_registers - 1] = 1;
 					}
 					graphics[x + x_line + ((y + y_line) * 64)] ^= 1;
 				}
@@ -300,78 +298,6 @@ private:
 		}
 		draw_flag = true;
 	}
-
-	inline void oneEqualTwo()
-	{
-		registers[register_index1] = registers[register_index2];
-	}
-
-	inline void oneEqualOneOrTwo()
-	{
-		registers[register_index1] = registers[register_index2] | registers[register_index1];
-	}
-
-	inline void oneEqualOneAndTwo()
-	{
-		registers[register_index1] = registers[register_index2] & registers[register_index1];
-	}
-
-	void oneEqualOneXorTwo()
-	{
-		registers[register_index1] = registers[register_index2] ^ registers[register_index1];
-	}
-
-	void oneEqualOnePlusTwo()
-	{
-		// max value is 0xFF. if value stored in index2 is greater than 0xFF minus value stored in index1 we have a carry
-		registers[number_of_registers - 1] = 0;
-		if (registers[register_index2] > 0xFF - registers[register_index1]) {
-			registers[number_of_registers - 1] = 1;// set carry
-		}
-		registers[register_index1] += registers[register_index2];
-	}
-
-	void oneEqualOneMinusTwo()
-	{
-		// if value in register_index2 is larger than value stored at register_index1 then we have a borrow
-		registers[number_of_registers - 1] = 1;
-		if (registers[register_index2] > registers[register_index1]) {
-			registers[number_of_registers - 1] = 0;// set borrow
-		}
-		registers[register_index1] -= registers[register_index2];
-	}
-
-	void oneEqualRightShiftOne()
-	{
-		// Shifts value at index1 right by one. The last register is set to the value of the least significant bit of the register with index1 before the shift
-		//  We store least significant bit of
-		registers[number_of_registers-1] = registers[register_index1] & 0x1;
-		registers[register_index1] >>=1;
-	}
-
-	void oneEqualLeftShiftOne()
-	{
-		// Shifts value at index1 right by one. The last register is set to the value of the most significant bit of the register with index1 before the shift
-		//  We store most significant bit of
-		registers[number_of_registers-1] = registers[register_index1] >> 7;
-		registers[register_index1] <<=1;
-	}
-
-	void oneEqualTwoMinusOne()
-	{
-		registers[number_of_registers - 1] = 1;
-		if (registers[register_index1] > registers[register_index2]) {
-			registers[number_of_registers - 1] = 0;// set borrow
-		}
-		registers[register_index1] = registers[register_index2] - registers[register_index1];
-	}
-
-	void skipNextInstructionIfRegisterValueAreNotEqual()
-	{
-		if(registers[register_index1] != registers[register_index2])
-			skip_instruction = true;
-	}
-
 
 	void loadSpritesToMemory()
 	{
@@ -403,7 +329,7 @@ private:
 	std::array<Bit8, number_of_stack_levels> stack{};
 	std::array<Bit8, number_of_keys> keypad{};
 	std::unordered_map<int, std::function<void()>> opcodeMap;
-	std::unordered_map<int, std::function<void(const int register1, const int register2)>> twoRegisterOperationsMap;
+	std::unordered_map<int, std::function<void()>> twoRegisterOperationsMap;
 
 	Bit16 current_opcode{};
 	Bit16 index_register{};
